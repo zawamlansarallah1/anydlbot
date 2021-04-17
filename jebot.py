0af7aee1bf26f34cd2ad1188d16ac4222cfad79f
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-04-18 00:51:44 +0530

    Update jebot.py

--- jebot.py
+++ jebot.py
@@ -236,7 +236,7 @@ else:
       basename = video_file.rsplit(".", 1)[-2]
       # thumbnail
       lel = "https://telegra.ph/file/7425967719f07e5512df5.jpg"
-      thumbnail_file = await wget.download(lel)
+      thumbnail_file = wget.download(lel)
       
       # info (s2tw)
       webpage_url = info_dict['webpage_url']
